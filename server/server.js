afcf5adcdbf7c82dd65e688e3cc28107d7983741
Alexeyca <maple111story@gmail.com>
2020-01-25 12:13:22 -0600

    server add

---
+++ server/server.js
@@ -0,0 +1,22 @@
+var express = require('express');
+var app = express();
+
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+app.get('/', function (req, res) {
+   res.sendFile( __dirname + "/" + "index.html" );
+})
+app.get('/style.css', function(req, res) {
+  res.sendFile(__dirname + "/" + "style.css");
+})
+app.get('/script.js', function(req, res) {
+  res.sendFile(__dirname + "/" + "script.js");
+});
+
+
+var server = app.listen(3000, function () {
+   var host = server.address().address
+   var port = server.address().port
+
+   console.log("Example app listening at http://%s:%s", host, port)
+})
