ded3b9ac77aa53e2353bb843dc4a42e7a570171a
Alexeyca <maple111story@gmail.com>
2020-01-25 21:19:13 -0600

    update

--- server.js
+++ server.js
@@ -18,5 +18,5 @@ var server = app.listen(3000, function () {
    var host = server.address().address
    var port = server.address().port
 
-   console.log("Example app listening at http://%s:%s", host, port)
+   console.log("app listening at http://%s:%s", host, port)
 })
