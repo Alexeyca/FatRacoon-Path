e8ea49f5905488f17b0d11e8ef660ea9719096a2
Alexeyca <maple111story@gmail.com>
2020-01-26 14:28:37 -0600

    mqtt update

--- script.js
+++ script.js
@@ -11,3 +11,25 @@ $('.projects').resizable();
 $('.devices').resizable();
 $('.main').resizable();
 $('.propertys').resizable();
+
+if ( window.sessionStorage.clientId){
+    clientName = window.sessionStorage.clientId;
+}else{
+    clientName = "mqtt_js_" + parseInt(Math.random()*100000, 10);
+    window.sessionStorage.clientId=clientName;
+}
+
+function connectMqtt(){
+    clientMqtt = new Paho.Mqtt.Client(hostname, Number(port), clientName);
+    console.info('Connecting to Mqtt, host : ', hostname, '. port: ', port, 'Client Name: ',clientName);
+    connectMqtt.onConnectionLost = onConnectionLost;
+    connectMqtt.onMessageArrived = onMessageArrived;
+
+    var options = {onSuccess: onConnect,
+        onFailure: onFail,
+    debugging
+        useSSL:true
+    };
+    connectMqtt.connect(options);
+    console.info('Connecting...')
+}
