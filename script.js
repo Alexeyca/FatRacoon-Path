08e91651f2414f764f8ee3beb2d112943604f506
Alexeyca <maple111story@gmail.com>
2020-01-26 13:23:10 -0600

    update

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
+var client;
+
+//mqtt client settings
+var hostname = "192.168.100.124";
+var port ="1883";
+var topics="#";
+var clientName;
+
 $('.menu').resizable();
 $('.projects').resizable();
 $('.devices').resizable();
